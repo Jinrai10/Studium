62779f1176b9c6fb5fd18e137a1b2302adefc5aa
Jinrai10 <Jinrai404@gmail.com>
2018-09-27 13:28:58 +0200

    new tasks

--- Vorkurs/day8.c
+++ Vorkurs/day8.c
@@ -2,28 +2,26 @@
 #include<stdlib.h>
 #include<string.h>
 
-
-int main(int args, char ** argv) {
-	
+int uniq(char * a, char * b) {
 	char buffer[256];
 	char buffer1[256];
 	int enthalten = 0;
 	
-	FILE * fd = fopen(argv[1], "r");
-	FILE * fg;
-	
+	FILE * fd = fopen(a, "r");
+	FILE * fg = fopen(b, "a+");
+	fclose(fg);
 	if (fd == NULL || fg == NULL) {
 		exit(0);
 	}
 	while(fgets(buffer, 255, fd) != NULL) {	
-		fg = fopen(argv[2], "r+");
+		fg = fopen(b, "r+");
 	
 		enthalten = 0;
 		while(enthalten == 0 && fgets(buffer1, 255, fg) != NULL) {
 			if (strcmp(buffer, buffer1) == 0) enthalten = 1;
 		}
 		if(enthalten == 0) {
-			fprintf(fg, "%s", buffer); 
+			fprintf(fg, "%s", buffer);
 			printf("%s", buffer);
 		}
 		fclose(fg);
@@ -31,4 +29,8 @@ int main(int args, char ** argv) {
 	fclose(fd);
 	
 	return 0;
+}
+
+int main(int args, char ** argv) {
+	uniq(argv[1], argv[2]);
 }

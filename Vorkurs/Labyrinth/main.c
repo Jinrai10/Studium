b5495b0576497c3b2b4be2f2b46c7f77cc0fe240
simon.liessem <simon.liessem@fkie.fraunhofer.de>
2018-09-28 16:03:00 +0200

    nearly done lab

--- Vorkurs/Labyrinth/main.c
+++ Vorkurs/Labyrinth/main.c
@@ -39,29 +39,75 @@ Position findPositionOf(int sizeX, int sizeY, char arr[sizeX][sizeY], char x) {
 	return point;
 }
 
+Position findNextFreePosition(int sizeX, int sizeY, char arr[sizeX][sizeY]) {
+	Position point;
+	
+	if (arr[0][1] == ' ') {
+		point.posX = 0;
+		point.posY = 1;
+		return point;
+	}
+	if (arr[1][2] == ' ') {
+		point.posX = 1;
+		point.posY = 2;
+		return point;
+	}
+	if (arr[1][0] == ' ') {
+		point.posX = 1;
+		point.posY = 0;
+		return point;
+	}
+	if (arr[2][1] == ' ') {
+		point.posX = 2;
+		point.posY = 1;
+		return point;
+	}
+	
+	point.posX = -1;
+	point.posY = -1;
+	
+	return point;
+}
+
 void getSurroundings(int sizeX, int sizeY, char arr[sizeX][sizeY], Position *pos, char surroundings[3][3]) {
 	for (int i = 0;i<3;i++) {
     	for (int j = 0;j<3;j++) {
+    		if (i == 1 && j == 1 && surroundings[1][1] == ' ') { 
+    			surroundings[1][1] = '-'; 
+	    		continue;
+    		}
     		surroundings[i][j] = arr[i-1+pos->posX][j-1+pos->posY];
     	}
     }
 	
 }
 
-int findPath(int sizeX, int sizeY, char labyrinth[sizeX][sizeY], 
-				Position *start, Position *final, char surroundings[3][3]) {
+int findPath(int sizeX, int sizeY, char labyrinth[sizeX][sizeY], Position *start, char target) {
 	Position freespot;
+	Position waypoint;
+	char surroundings[3][3];
+		
+	getSurroundings(sizeX, sizeY, labyrinth, start, surroundings);
 	
-	printarr(3, 3, surroundings);
+	if(findPositionOf(3, 3, surroundings, target).posX != -1) return 0;
 	
-	freespot = findPositionOf(3, 3, surroundings, ' ');
 	
-	if (freespot.posX != -1) {
-		surroundings[start->posX][start->posY] = '-';
-		findPath(sizeX, sizeY, labyrinth, &freespot, final, surroundings);
+	while(freespot.posX != -1) {
+		freespot = findNextFreePosition(3, 3 ,surroundings);
+		printarr(3, 3, surroundings);
+		printf("%i_%i\n", start->posX, start->posY);
+		printf("%c\n", surroundings[2][1]);
+		
+		if (freespot.posX == -1) return 1;
+		surroundings[freespot.posX][freespot.posY] = '-';
+		waypoint.posX = start->posX+freespot.posX-1;
+		waypoint.posY = start->posY+freespot.posY-1;
+		if (findPath(sizeX, sizeY, labyrinth, &waypoint, target) == 0) {
+			printf("Weg gefunden!");
+			labyrinth[freespot.posX][freespot.posY] = '.';
+		}
+		
 	}
-   	
-    return 0;
 }
 
 void loadLab(char *a, int sizeX, int sizeY, char lab[sizeX][sizeY]) {
@@ -93,13 +139,8 @@ int solveLabyrith(char *a, int sizeX, int sizeY) {
     printarr(sizeX, sizeY, labyrinth);
     
     Position start = findPositionOf(sizeX, sizeY, labyrinth, '$');
-    Position final = findPositionOf(sizeX, sizeY, labyrinth, '*');
     
-	char surroundings[3][3];
-    
-    getSurroundings(sizeX, sizeY, labyrinth, &start, surroundings);
-    
-    findPath(sizeX, sizeY, labyrinth, &start, &final, surroundings);
+    findPath(sizeX, sizeY, labyrinth, &start, '*');
   
     printarr(sizeX, sizeY, labyrinth);
     return 0;

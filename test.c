29398f61d15fc3fdb41048192c51809c68605448
Simon <Jinrai404@gmail.com>
2018-12-13 13:58:23 +0100

    stuff

--- test.c
+++ test.c
@@ -17,5 +17,15 @@ int main()
     int y = function(x);
     // der ganzzahl y wird der Rückgabewert der Funktion function mit dem Parameter x zugewiesen
     printf("%i\n", y);
+
+    int * b = NULL;
+    int c = 5;
+    int *zeiger = &c;
+    b = zeiger;
+    (*b)+=1;
+    printf("Pointer: %p\nValue: %i\n",zeiger, *zeiger );
+    printf("Pointer: %p\nValue: %i\n",b, *b );
+    //printf("%i_%i\n", *a, *b);
+
     return 0;
 }

6b191e5749c746e2542f70f6419b8fef21a44fb6
Jinrai10 <Jinrai404@gmail.com>
2019-03-26 20:21:03 +0100

    added fraunhofer Dir

---
+++ fraunhofer/ntp-client.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+from ntp_packet import NTPPacket, NTPTimestamp
+
+HOST, PORT = "127.0.1.1", 9999
+data = "".join(sys.argv[1:])
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
+received = NTPPacket().from_bytes(sock.recv(1024))
+
+print("Sent:        {}".format(data))
+print("Received:    {}".format(received))
